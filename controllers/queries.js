4b5215bcdd818c03d9e352a131f870f64185c742
wu0010802 <139047133+wu0010802@users.noreply.github.com>
2024-02-04 13:10:49 -0800

    get user info

--- controllers/queries.js
+++ controllers/queries.js
@@ -1,22 +1,44 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: 'yilunwu',
-    host: 'localhost',
-    database: 'user',
-    password: 'password',
-    port: 5432,
+  user: 'yilunwu',
+  host: 'localhost',
+  database: 'user',
+  password: 'password',
+  port: 5432,
+})
+
+
+const get_users_info = (request, response) => {
+  pool.query('SELECT * FROM user_info ORDER BY user_id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows);
+  });
+}
+
+
+const get_users_info_by_name = (request, response) => {
+  const name = request.params.name;
+
+  pool.query('SELECT * FROM user_info WHERE name = $1', [name], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+
+const post_user_info = (request, response) => {
+  const { name, weight, height, year } = request.body
+
+  pool.query('INSERT INTO user_info (name, weight,height ,year) VALUES ($1,$2,$3,$4) RETURNING *', [name, weight, height, year], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${results.rows[0].user_id}`)
   })
-
-
-const post_user_info = (request,response)=>{
-    const {name, weight,height ,year} = request.body
-
-    pool.query('INSERT INTO user_info (name, weight,height ,year) VALUES ($1,$2,$3,$4) RETURNING *' , [name, weight,height ,year], (error, results) => {
-        if (error) {
-          throw error
-        }
-        response.status(201).send(`User added with ID: ${results.rows[0].user_id}`)
-      })
 }
 
 
@@ -27,5 +49,7 @@ const post_user_info = (request,response)=>{
 
 
 module.exports = {
-    post_user_info,
-  }
+  post_user_info,
+  get_users_info,
+  get_users_info_by_name,
+}
--- index.js
+++ index.js
@@ -6,16 +6,19 @@ const port = 3000;
 
 app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-      extended: true,
-    })
-  )
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
-app.post('/users',db.post_user_info);
+
+app.get('/users', db.get_users_info);
+app.get('/users/:name', db.get_users_info_by_name);
+app.post('/users', db.post_user_info);
 
 
 
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-  })
+  console.log(`App running on port ${port}.`)
+})
