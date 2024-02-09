50475726a81bdea4cc5f6e0035299e94a7693bd7
wu0010802 <139047133+wu0010802@users.noreply.github.com>
2024-02-08 20:23:11 -0800

    nutriton complete

--- controllers/intake.js
+++ controllers/intake.js
@@ -10,12 +10,9 @@ const pool = new Pool({
     port: 5432,
 })
 
-// 加入食物至intakelogs(user_id,food_id,intake_date,amount)
+
 const post_intake = async (request, response, next) => {
     const {user_id,food_name,amount}= request.body;
-    // const food_name = request.params.food;
-    // const user_id = request.params.user_id;//默認1
-    // const amount = request.params.amount;
     const now = new Date();
     try {
         const result_id = await pool.query('SELECT food_id FROM food_info WHERE food_name = $1', [food_name]);
@@ -27,41 +24,12 @@ const post_intake = async (request, response, next) => {
             response.status(201).send(`User added with intake: ${result.rows[0].food_id}`);
            }
         });
-
     }
     catch (error) {
         console.error(error);
-        // 調用 next 函數傳遞錯誤到 Express 的錯誤處理中間件
         next(error);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const daily_target_calculate = async(request,response,next) =>{
-
-// }
-
-
-
-
-
-
-
-
-
-
 
 module.exports ={
     post_intake
--- controllers/userqueries.js
+++ controllers/userqueries.js
@@ -8,6 +8,57 @@ const pool = new Pool({
   port: 5432,
 })
 
+class Nutrition {
+  constructor(tdee) {
+    this.tdee = tdee;
+
+  }
+  target_protein (){
+    const protein = this.tdee * 0.275/4;
+    return Number(protein.toFixed(2));
+  }
+  target_fat (){
+    const fat = this.tdee * 0.2/9;
+    return Number(fat.toFixed(2));
+  }
+  target_carbohydrate (){
+    const target_carbohydrate = this.tdee * 0.525/4;
+    return Number(target_carbohydrate.toFixed(2));
+  }
+
+}
+
+
+function BMR_calculate(gender, weight, height, age) {
+  let bmr;
+  if (gender === "male") {
+    bmr = 10 * weight + 6.25 * height - (5 * age) + 5;
+  } else {
+    bmr = 10 * weight + 6.25 * height - (5 * age) - 161;
+  }
+  return bmr
+}
+
+function TDEE_calculate(exercise_per_week, user_bmr) {
+  let tdee;
+  if (exercise_per_week === 0) {
+    tdee = user_bmr * 1.2;
+  } else if (exercise_per_week <= 3) {
+    tdee = user_bmr * 1.375;
+  } else if (exercise_per_week <= 5) {
+    tdee = user_bmr * 1.55;
+  } else {
+    tdee = user_bmr * 1.725;
+  }
+  return Number(tdee.toFixed(2));
+}
+
+function bmi_calculate(weight, height) {
+  const bmi = weight / (height / 100) ** 2;
+  return Number(bmi.toFixed(2))
+}
+
+
 const get_users_info = (request, response) => {
   pool.query('SELECT * FROM user_info ORDER BY user_id ASC', (error, results) => {
     if (error) {
@@ -28,27 +79,38 @@ const get_users_info_by_name = (request, response) => {
   });
 };
 
-const post_user_info = (request, response, next) => {
-  const { name, height, weight, year, gender } = request.body;
-  // const bmi = weight / (height / 100) ** 2;
-  const now = new Date();
 
-  pool.query('INSERT INTO user_info (name, weight, height, year, gender, date) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
-    [name, height, weight, year, gender, now], (error, results) => {
+const post_user_info = (request, response, next) => {
+  const { name, height, weight, age, gender, exercise_per_week } = request.body;
+  const bmr = BMR_calculate(gender, weight, height, age);
+  const bmi = bmi_calculate(weight, height);
+  const now = new Date();
+  const tdee = TDEE_calculate(exercise_per_week, bmr);
+  const nutrition = new Nutrition(tdee);
+  const target_protein = nutrition.target_protein();
+  const target_fat = nutrition.target_fat();
+  const target_carbohydrate = nutrition.target_carbohydrate();
+
+  pool.query('INSERT INTO user_info (name, weight, height, age, gender, date,bmi,tdee,protein,fat,carbohydrates) VALUES ($1, $2, $3, $4, $5, $6,$7,$8,$9,$10,$11) RETURNING *',
+    [name, height, weight, age, gender, now, bmi, tdee,target_protein,target_fat,target_carbohydrate], (error, results) => {
       if (error) {
         next(error);
       } else {
-        response.status(201).send(`User added with ID: ${results.rows[0].user_id}`);
+        response.status(201).send(`User added with ID: ${results.rows[0].name}`);
       }
     });
 };
 
 const update_user_info = (request, response, next) => {
   const name = request.params.name;
-  const { weight, height, year, gender } = request.body;
-  // const bmi = weight / (height / 100) ** 2;
-  const now = new Date();
-  pool.query('UPDATE user_info SET weight = $2,gender=$5 ,height = $3, year = $4 WHERE name = $1 RETURNING *', [name, weight, height, year, gender], (error, results) => {
+  const { weight, height, age, gender, exercise_per_week } = request.body;
+
+  const bmr = BMR_calculate(gender, weight, height, age);
+  const bmi = bmi_calculate(weight, height);
+  const tdee = TDEE_calculate(exercise_per_week, bmr);
+
+
+  pool.query('UPDATE user_info SET weight = $2,gender=$5 ,height = $3, age = $4,bmi =$6,tdee = $7 WHERE name = $1 RETURNING *', [name, weight, height, age, gender, bmi, tdee], (error, results) => {
     if (error) {
       next(error);
     }
