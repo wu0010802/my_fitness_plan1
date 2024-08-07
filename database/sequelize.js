418c33c3a71be7e80933fc38056b17a3cd1178f8
wu0010802 <139047133+wu0010802@users.noreply.github.com>
2024-08-07 22:35:11 +0800

    deploy test

--- database/sequelize.js
+++ database/sequelize.js
@@ -25,18 +25,7 @@ const sequelize = isProduction
     }
     
   );
-// const sequelize = new Sequelize(
-//   process.env.local_postgresql_DATABASE,
-//   process.env.local_postgresql_USER,
-//   process.env.local_postgresql_PASSWORD,
-//   {
-//     host: process.env.local_postgresql_HOST,
-//     port: process.env.local_postgresql_PORT,
-//     dialect: process.env.local_sql_type,
-    
-//   }
-  
-// );
+
 module.exports = sequelize;
 
 
