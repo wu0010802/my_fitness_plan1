ff6491d261befeb090df54e67ed93ea4903a32ae
wu0010802 <139047133+wu0010802@users.noreply.github.com>
2024-08-07 12:15:14 +0800

    login bug fix

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,30 +1,43 @@
-require('dotenv').config();
-
+require('dotenv').config({ path: '.env.dev' });
 const { Sequelize } = require('sequelize');
-const isProduction = process.env.NODE_ENV === 'production';
 
-const sequelize = isProduction
-  ? new Sequelize(process.env.DATABASE_URL, {
-      dialect: 'postgres',
-      protocol: 'postgres',
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-      }
-    })
-  : new Sequelize(
-      process.env.local_postgresql_DATABASE,
-      process.env.local_postgresql_USER,
-      process.env.local_postgresql_PASSWORD,
-      {
-        host: process.env.local_postgresql_HOST,
-        dialect: process.env.local_sql_type,
-        port: process.env.local_postgresql_PORT
-      }
-    );
+// const isProduction = process.env.NODE_ENV === 'production';
 
+// const sequelize = isProduction
+//   ? new Sequelize(process.env.DATABASE_URL, {
+//       dialect: 'postgres',
+//       protocol: 'postgres',
+//       dialectOptions: {
+//         ssl: {
+//           require: true,
+//           rejectUnauthorized: false
+//         }
+//       }
+//     })
+//   : new Sequelize(
+//     process.env.local_postgresql_DATABASE,
+//     process.env.local_postgresql_USER,
+//     process.env.local_postgresql_PASSWORD,
+//     {
+//       host: process.env.local_postgresql_HOST,
+//       port: process.env.local_postgresql_PORT,
+//       dialect: process.env.local_sql_type,
+      
+//     }
+    
+//   );
+const sequelize = new Sequelize(
+  process.env.local_postgresql_DATABASE,
+  process.env.local_postgresql_USER,
+  process.env.local_postgresql_PASSWORD,
+  {
+    host: process.env.local_postgresql_HOST,
+    port: process.env.local_postgresql_PORT,
+    dialect: process.env.local_sql_type,
+    
+  }
+  
+);
 module.exports = sequelize;
 
 
