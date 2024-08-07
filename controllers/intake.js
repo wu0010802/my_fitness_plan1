6f95048e8705a6e7c98a31de5d093cc01feb17fc
wu0010802 <139047133+wu0010802@users.noreply.github.com>
2024-08-07 23:53:17 +0800

    test deploy

--- controllers/intake.js
+++ controllers/intake.js
@@ -3,9 +3,51 @@ const { request, response } = require('express');
 const UserInfo = require('../models/UserInfo');
 
 console.log('Before requiring IntakeLogs');
-const IntakeLogs = require('../models/IntakeLogs');
+// const IntakeLogs = require('../models/IntakeLogs');
 console.log('After requiring IntakeLogs');
 const FoodInfo = require('../models/FoodInfo');
+
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/sequelize');
+const UserInfo = require('./UserInfo');
+const FoodInfo = require('./FoodInfo');
+
+const IntakeLogs = sequelize.define('IntakeLogs', {
+    log_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: UserInfo,
+            key: 'user_id'
+        }
+    },
+    food_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: FoodInfo,
+            key: 'food_id'
+        }
+    },
+    amount: {
+        type: DataTypes.DOUBLE,
+        allowNull: false
+    },
+    date: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    }
+}, {
+    tableName: 'intakelogs',
+    timestamps: false
+});
+
 
 
 UserInfo.belongsToMany(FoodInfo, { through: IntakeLogs, foreignKey: 'user_id' });
